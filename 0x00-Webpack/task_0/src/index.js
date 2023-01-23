47989f15cf85e2f13bd4da76e9096a0db2d72e3a
chrisokwisa <okwisachristabel@gmail.com>
2023-01-23 09:33:05 +0000

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(document).ready(function(){
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
