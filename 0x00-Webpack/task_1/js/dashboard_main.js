468ec14e8f9754801045d35484b70dc959283727
chrisokwisa <okwisachristabel@gmail.com>
2023-01-26 10:36:33 +0000

    Using debounce

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -11,7 +11,7 @@ let count = 0;
 
 function updateCounter() {
   count++;
-  $("#count").html(`${count} clicks on the button`);
+  $("#count").text(`${count} clicks on the button`);
 };
 
 $('button').on('click', _.debounce(updateCounter, 1000));
