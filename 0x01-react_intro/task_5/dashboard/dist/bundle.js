bfcedf2e4c109fc3d8bbebab11c397d35705c28b
chrisokwisa <okwisachristabel@gmail.com>
2023-01-27 09:51:22 +0000

    Testing

--- 0x01-react_intro/task_5/dashboard/dist/bundle.js
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -1,6 +1,25 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'fs/promises'\nRequire stack:\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/babel-loader/lib/cache.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/babel-loader/lib/index.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/loader-runner/lib/loadLoader.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/loader-runner/lib/LoaderRunner.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack/lib/NormalModuleFactory.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack/lib/Compiler.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack/lib/webpack.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack/lib/index.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack-cli/lib/webpack-cli.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack-cli/lib/bootstrap.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack-cli/bin/cli.js\n- /alx-react/0x01-react_intro/task_5/dashboard/node_modules/webpack/bin/webpack.js\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:667:27)\n    at Module.require (internal/modules/cjs/loader.js:887:19)\n    at require (internal/modules/cjs/helpers.js:74:18)\n    at Object.<anonymous> (/alx-react/0x01-react_intro/task_5/dashboard/node_modules/babel-loader/lib/cache.js:24:5)\n    at Module._compile (internal/modules/cjs/loader.js:999:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\n    at Module.load (internal/modules/cjs/loader.js:863:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\n    at Module.require (internal/modules/cjs/loader.js:887:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUNBQTtVQUNBO1VBQ0E7VUFDQTtVQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZGFzaGJvYXJkL3dlYnBhY2svYmVmb3JlLXN0YXJ0dXAiLCJ3ZWJwYWNrOi8vZGFzaGJvYXJkL3dlYnBhY2svc3RhcnR1cCIsIndlYnBhY2s6Ly9kYXNoYm9hcmQvd2VicGFjay9hZnRlci1zdGFydHVwIl0sInNvdXJjZXNDb250ZW50IjpbIiIsIi8vIHN0YXJ0dXBcbi8vIExvYWQgZW50cnkgbW9kdWxlIGFuZCByZXR1cm4gZXhwb3J0c1xuLy8gVGhpcyBlbnRyeSBtb2R1bGUgZG9lc24ndCB0ZWxsIGFib3V0IGl0J3MgdG9wLWxldmVsIGRlY2xhcmF0aW9ucyBzbyBpdCBjYW4ndCBiZSBpbmxpbmVkXG52YXIgX193ZWJwYWNrX2V4cG9ydHNfXyA9IHt9O1xuX193ZWJwYWNrX21vZHVsZXNfX1tcIi4vc3JjL2luZGV4LmpzXCJdKCk7XG4iLCIiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import Notifications from "./Notifications";
+import App from "./App/App";
+import Notifications from "./Notifications/Notifications";
 
 ReactDOM.render(
   <React.StrictMode>
+    <div id="root-notifications">
+      <Notifications />
+    </div>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Notifications />
-  </React.StrictMode>,
-  document.getElementById("root-notifications")
 );
