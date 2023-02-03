594c6894a243a16b491b66108f76882f1ffc9e3a
chrisokwisa <okwisachristabel@gmail.com>
2023-02-03 14:21:21 +0300

    shapes, keys and loops

--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -4,6 +4,12 @@ import { getLatestNotification } from "../utils/utils";
 import Notifications from "./Notifications";
 import NotificationItem from "./NotificationItem";
 
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
 describe("Notification tests", () => {
   it("renders Notification component without crashing", () => {
     const wrapper = shallow(<Notifications />);
@@ -12,8 +18,15 @@ describe("Notification tests", () => {
   });
 
   it("renders correct list items", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
+    const wrapper = shallow(
+      <Notifications
+        displayDrawer={true}
+        listNotifications={listNotifications}
+      />
+    );
+    expect(wrapper.find("ul").children()).toHaveLength(
+      listNotifications.length
+    );
     wrapper.find("ul").forEach((node) => {
       expect(node.equals(<NotificationItem />));
     });
@@ -28,19 +41,11 @@ describe("Notification tests", () => {
     );
   });
 
-  it("renders an unordered list", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
-    wrapper.find("ul").forEach((node) => {
-      expect(node.equals(<NotificationItem />));
-    });
-  });
-
   it("renders correct text", () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
 
     expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBe(
-      true
+      false
     );
   });
 
@@ -49,7 +54,7 @@ describe("Notification tests", () => {
 
     expect(wrapper.find("div.menuItem").exists()).toBe(true);
     expect(wrapper.find("div.menuItem").html()).toEqual(
-      '<div class="menuItem"><p>Your notifications</p></div>'
+      '<div class="menuItem"><p>Your Notifications</p></div>'
     );
   });
 
@@ -69,5 +74,43 @@ describe("Notification tests", () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
 
     expect(wrapper.find("div.Notifications").exists()).toBe(true);
+  });
+
+  it("renders correctly when listCourses is not passed", () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
+  });
+
+  it("renders correctly when empty array is passed", () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
+  });
+
+  it('renders "No new notifications for now"  when listNotifications is empty', () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
+    ).toBe(false);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
   });
 });
