14fddafc623ea1e9cd0d8e17303de61b16de9b0b
chrisokwisa <okwisachristabel@gmail.com>
2023-02-06 12:10:42 +0000

    Tests

--- 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -44,7 +44,7 @@ describe("CourseList component tests", () => {
     );
   });
 
-  it("renders correctely when passed a list of courses", () => {
+  it("renders correctly when passed a list of courses", () => {
     const wrapper = shallow(<CourseList listCourses={listCourses} />);
 
     expect(wrapper.find("tbody").children()).toHaveLength(3);
