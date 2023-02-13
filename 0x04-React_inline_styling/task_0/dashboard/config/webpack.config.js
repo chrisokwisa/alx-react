dba4662d77a9ef36d237ca6d5ce4c053fce893c6
chrisokwisa <okwisachristabel@gmail.com>
2023-02-13 12:06:07 +0300

    inline styling

--- 0x04-React_inline_styling/task_0/dashboard/config/webpack.config.js
+++ 0x04-React_inline_styling/task_0/dashboard/config/webpack.config.js
@@ -1,20 +1,11 @@
-const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  testEnvironment: "jsdom",
-  mode: "development",
-  devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve("./dist"),
   },
-  devServer: {
-    hot: true,
-    contentBase: path.resolve("./dist"),
-    compress: true,
-    port: 3000,
-  },
+  mode: "development",
   module: {
     rules: [
       {
@@ -22,25 +13,42 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpg|png)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource',
         use: [
           "file-loader",
           {
             loader: "image-webpack-loader",
             options: {
-              bypassOnDebug: true,
-              disable: true,
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
       },
       {
-        test: /\.jsx?$/i,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ["babel-loader"],
       },
     ],
   },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8564,
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: "index.html",
+      inject: false,
+      template: "./dist/index.html",
+    }),
+  ],
 };
