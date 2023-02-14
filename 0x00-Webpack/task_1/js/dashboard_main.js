40e2b98b06f3cfb693586ee99cd29e9bff221377
chrisokwisa <okwisachristabel@gmail.com>
2023-02-14 13:24:20 +0300

    Correction of Webpack files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,11 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+let conunt = 0;
 
 function updateCounter() {
   count++;
-  $("#count").text(`${count} clicks on the button`);
+  $("#count").html(`${count} clicks on the button`);
 };
 
-$('button').on('click', _.debounce(updateCounter, 1000));
+$('button').on('click', _.debounce(updateCounter, 500));
