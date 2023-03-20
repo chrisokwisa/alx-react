ec0a0f8fcf1d85bb935560e90952db89b3145b9b
chriskwisa <okwisachristabel@gmail.com>
2023-03-20 11:51:25 +0300

    Connect notifications: New Action Creator

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/selectors/notificationSelector.js
@@ -0,0 +1,17 @@
+import { Map } from "immutable";
+
+export const filterTypeSelected = (state) => state.get("filter");
+
+export const getNotifications = (state) => state.notifications;
+
+export const getUnreadNotifications = (state) => {
+  const notifications = state.getIn([
+    "notifications",
+    "entities",
+    "notifications",
+  ]);
+  const unreadNotifications = notifications.filter(
+    (notification) => !notification.isRead
+  );
+  return Map(unreadNotifications);
+};
