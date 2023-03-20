f66d3bf7f1e959215eeb321a041ece9103afc02a
chriskwisa <okwisachristabel@gmail.com>
2023-03-20 13:41:25 +0300

    connect courses: create a fetch courses function

--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
@@ -1,6 +1,8 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { SELECT_COURSE, UNSELECT_COURSE, FETCH_COURSE_SUCCESS } from "./courseActionTypes";
 
-export function selectCourse(index) {
+import "node-fetch";
+
+export const selectCourse = (index) => {
   return {
     type: SELECT_COURSE,
     index: index,
@@ -17,3 +19,19 @@ export function unSelectCourse(index) {
 }
 
 export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
+
+export const setCourses = (data) => {
+  return {
+    type: FETCH_COURSE_SUCCESS,
+    data,
+  };
+};
+
+export const fetchCourses = () => {
+  return (dispatch) => {
+    return fetch("./courses.json")
+    .then((res) => res.json())
+    .then((data) => dispatch(setCourses))
+    .catch((error) => {});
+  };
+};
